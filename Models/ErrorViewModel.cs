aa6da5aa569cac1de1c5dd96826b63560da4af3d
RicardoVilla <RicardoVilla@SYSTEMPC2>
2023-01-10 09:18:11 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Appoints.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
